a3a630c38682f0a89513dbb4495d6bea12af0fc9
Ramesh Prasad <rp181135198@gmail.com>
2021-11-19 19:06:27 +0530

    Update : Find prime numbers between a range of numbers.

--- arithmetic/prime_number_range.cpp
+++ arithmetic/prime_number_range.cpp
@@ -35,9 +35,7 @@ void primeNumberRange(int lowerBound, int upperBound)
         lowerBound++;
 
     if (upperBound % 2 == 0)
-    {
         upperBound--;
-    }
 
     for (int i = lowerBound; i <= upperBound; i += 2)
     {
@@ -49,41 +47,6 @@ void primeNumberRange(int lowerBound, int upperBound)
     cout << endl;
 }
 
-/*
-// Not work correctly : Need debugging
-void primeNumberRange(int lowerBound, int upperBound)
-{
-    int l = lowerBound / 6;
-    int h = upperBound / 6;
-    if (lowerBound <= 2) {
-        lowerBound = 2;
-        if (upperBound == 2) {
-            cout << lowerBound ;
-        } else if (upperBound > 2) {
-            cout << lowerBound << ", " << (++lowerBound) << ", ";
-        }
-    }
-
-    if(((6*l + 1) < lowerBound) || ((6*l - 1) < lowerBound)) {
-        l++;
-    }
-
-    for (int i = l; i <= h; i++)
-    {
-        int a = 6*i-1;
-        int b = 6*i+1;
-        bool flagA = isPrime(a);
-        bool flagB = isPrime(b);
-        if(flagA) {
-            cout<<a<<", ";
-        }
-        if(flagB) {
-            cout<<b<<", ";
-        }
-    }
-    cout<<endl;
-}
-*/
 int main()
 {
     int lowerBound, upperBound;
@@ -91,12 +54,6 @@ int main()
     cin >> lowerBound;
     cout << "Enter the upper boundary : ";
     cin >> upperBound;
-    // primeNumberRange(lowerBound, upperBound);
-    for (int i = -3; i <= lowerBound; i++)
-    {
-        cout << "For lowerBound=" << i << " : ";
-        primeNumberRange(i, upperBound);
-        cout << endl;
-    }
+    primeNumberRange(lowerBound, upperBound);
     return 0;
 }
